90613974e9d9498f73e12024d9887b02830fa648
Alex Shatokhin <shatokhin.04@inbox.ru>
2024-04-01 10:52:57 +0300

    create black theme

--- src/ts/gameRenderer/SwitchPageTheme.ts
+++ src/ts/gameRenderer/SwitchPageTheme.ts
@@ -1,6 +1,7 @@
+type ThemeType = "white" | "black";
 
 class SwitchPageTheme {
-    private theme = "white"
+    static theme : ThemeType = "white"
     public renderThemeButton(){
         const themeButton = document.createElement("button");
         themeButton.classList.add("menu-content__button", "theme-button");
@@ -13,9 +14,28 @@ class SwitchPageTheme {
     }
 
     private switchTheme(){
-        const newTheme = this.theme === "white" ? "black" : "white"
-        document.documentElement.style.backgroundColor = newTheme;
-        this.theme = newTheme;
+        const newTheme : ThemeType = SwitchPageTheme.theme === "white" ? "black" : "white"
+        document.documentElement.style.backgroundColor = newTheme === "white" ? "#FFFFFF" : "#404040";        
+
+        const themeButtonsTitle = document.querySelector(".themes-switcher__title");
+        const menuContent = document.querySelector(".menu-content__information");
+        const board = document.querySelector(".board");
+
+        this.toggleElementsTheme(themeButtonsTitle, SwitchPageTheme.theme, newTheme);
+        this.toggleElementsTheme(menuContent, SwitchPageTheme.theme, newTheme);
+        this.toggleElementsTheme(board, SwitchPageTheme.theme, newTheme);
+
+        SwitchPageTheme.theme = newTheme;
+
+    }
+
+    private toggleElementsTheme(element: Element | null, oldTheme : ThemeType, newTheme : ThemeType){
+        element?.classList.remove(`theme_${oldTheme}`);
+        element?.classList.add(`theme_${newTheme}`);
+    }
+
+    static getTheme() : ThemeType{
+        return SwitchPageTheme.theme;
     }
 }
 
